8b78a9de2773e690e1970072d27222295368a5fc
Mark MacGillivray <mark@cottagelabs.com>
2016-05-17 23:20:29 +0100

    fix to package that had git info in it

---
+++ packages/meteor-restivus-mm-hold/package.js
@@ -0,0 +1,47 @@
+Package.describe({
+  name: 'restivus-mm',
+  summary: 'Create authenticated REST APIs in Meteor 0.9+ via HTTP/HTTPS. Setup CRUD endpoints for Collections.',
+  version: '0.8.10',
+  git: 'https://github.com/kahmali/meteor-restivus.git'
+});
+
+
+Package.onUse(function (api) {
+  // Minimum Meteor version
+  api.versionsFrom('METEOR@0.9.0');
+
+  // Meteor dependencies
+  api.use('check');
+  api.use('coffeescript');
+  api.use('underscore');
+  api.use('accounts-password@1.1.4');
+  api.use('json-routes-mm@2.1.0');
+
+  api.addFiles('lib/auth.coffee', 'server');
+  api.addFiles('lib/iron-router-error-to-response.js', 'server');
+  api.addFiles('lib/route.coffee', 'server');
+  api.addFiles('lib/restivus.coffee', 'server');
+
+  // Exports
+  api.export('Restivus', 'server');
+});
+
+
+Package.onTest(function (api) {
+  // Meteor dependencies
+  api.use('practicalmeteor:munit');
+  api.use('test-helpers');
+  api.use('nimble:restivus');
+  api.use('http');
+  api.use('coffeescript');
+  api.use('underscore');
+  api.use('accounts-base');
+  api.use('accounts-password');
+  api.use('mongo');
+
+  api.addFiles('lib/route.coffee', 'server');
+  api.addFiles('test/api_tests.coffee', 'server');
+  api.addFiles('test/route_unit_tests.coffee', 'server');
+  api.addFiles('test/authentication_tests.coffee', 'server');
+  api.addFiles('test/user_hook_tests.coffee', 'server');
+});
