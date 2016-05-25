dd46a069cc30ae58359c3b7764ccd3ac9f155ccc
Mark MacGillivray <mark@cottagelabs.com>
2016-05-25 16:01:22 +0100

    changing elasticsearch api code to better deal with mappings

--- apps/clapi/server/endpoints/elasticsearch.js
+++ apps/clapi/server/endpoints/elasticsearch.js
@@ -69,7 +69,7 @@ CLapi.internals.es.map = function(route,map,url) {
   if ( map === undefined ) map = Meteor.http.call('GET','http://static.cottagelabs.com/mapping.json').data;
   if ( route.indexOf('/') !== 0 ) route = '/' + route;
   var routeparts = route.substring(1,route.length).split('/');
-  var esurl = url ? url : Meteor.settings.es.url + route;
+  var esurl = url ? url : Meteor.settings.es.url;
   var db = esurl + '/' + routeparts[0];
   try {
     var dbexists = Meteor.http.call('HEAD',db);
@@ -77,24 +77,27 @@ CLapi.internals.es.map = function(route,map,url) {
     Meteor.http.call('POST',db);
   }
   var maproute = db + '/_mapping/' + routeparts[1];
-  if ( Meteor.settings.es.version > 1 ) maproute = db + '/' + routeparts[1] + '/_mapping';
+  if ( Meteor.settings.es.version < 1 ) maproute = db + '/' + routeparts[1] + '/_mapping';
   return Meteor.http.call('PUT',maproute,{data:map});
 }
 
 CLapi.internals.es.query = function(action,route,data,url) {
   console.log('Performing elasticsearch ' + action + ' on ' + route);
-  var esurl = url ? url : Meteor.settings.es.url + route;
+  var esurl = url ? url : Meteor.settings.es.url;
+  var routeparts = route.substring(1,route.length).split('/');
   // check if route to indextype exists, if it does not, autocreate with default mapping
-  if (route.indexOf('/_') === -1 && route.split('/').length === 4 && (action === 'POST' || action === 'PUT')) {
+  if (route.indexOf('/_') === -1 && routeparts.length >= 1 && (action === 'POST' || action === 'PUT')) {
     try {
-      var exists = Meteor.http.call('HEAD',esurl);
+      var turl = esurl + '/' + routeparts[0];
+      if (routeparts.length > 1) turl += '/' + routeparts[1];
+      var exists = Meteor.http.call('HEAD',turl);
     } catch(err) {
       CLapi.internals.es.map(route);
     }
   }
   var opts = {}
   if (data) opts.data = data;
-  return Meteor.http.call(action,esurl,opts).data;
+  return Meteor.http.call(action,esurl+route,opts).data;
 }
 CLapi.internals.es.insert = function(route,data,url) {
   return CLapi.internals.es.query('POST',route,data,url);
